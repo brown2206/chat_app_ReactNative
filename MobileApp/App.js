1e3fc66e778c1a8b5223c2119d7a036a9ed77e47
brown2206 <brown.angelo.jr@gmail.com>
2022-02-27 11:44:35 -0500

    initial test run

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,10 +1,17 @@
+import React, { useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import io from "socket.io-client";
 
 export default function App() {
+  
+  useEffect(function() {
+    io("http://127.0.0.1:3001")
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!, I love you!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
