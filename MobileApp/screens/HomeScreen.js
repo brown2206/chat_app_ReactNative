2baff82e72eae273049fbe9667e889b8e1ec8593
brown2206 <brown.angelo.jr@gmail.com>
2022-04-20 00:36:53 -0400

    installed gifted chat

--- MobileApp/screens/HomeScreen.js
+++ MobileApp/screens/HomeScreen.js
@@ -1,14 +1,31 @@
 import React, { useEffect, useState, useRef } from "react";
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import io from "socket.io-client";
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default function HomeScreen() {
   const [messageToSend, setMessageToSend] = useState("");
+  const [recvMessages, setRecvMessages] = useState([]);
   const socket = useRef(null);
 
   useEffect(() => {
     socket.current = io("http://10.0.0.191:3001");
-  }, [])
+    socket.current.on("message", message => {
+        setRecvMessages(prevState => [...prevState, message]);
+    });
+    setRecvMessages([
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ]);
+  }, []);
 
   const sendMessage = () => {
     socket.current.emit("message", messageToSend);
@@ -16,14 +33,17 @@ export default function HomeScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <TextInput 
-        value={messageToSend} 
-        onChangeText={text => setMessageToSend(text)} 
-        placeholder="Enter chat message.."
-        onSubmitEditing={sendMessage}
-      />
+    <View style={{flex: 1}}>
+    <GiftedChat
+        messages={recvMessages}
+        // onSend={messages => this.onSend(messages)}
+        user={{
+            _id: 1,
+        }}
+    />
+    {
+      Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />
+    }
     </View>
   );
 }
